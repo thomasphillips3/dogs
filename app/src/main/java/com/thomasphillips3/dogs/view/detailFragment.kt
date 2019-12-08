b7a5d47a42a704a6639b1a0740c057b598a7d905
Thomas Phillips <thomasphillips3@gmail.com>
2019-12-07 20:22:30 -0800

    implement navigation between fragments

--- app/src/main/java/com/thomasphillips3/dogs/view/detailFragment.kt
+++ app/src/main/java/com/thomasphillips3/dogs/view/detailFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.thomasphillips3.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -22,5 +25,13 @@ class detailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonList.setOnClickListener {
+            val action = detailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 
 }
--- app/src/main/java/com/thomasphillips3/dogs/view/listFragment.kt
+++ app/src/main/java/com/thomasphillips3/dogs/view/listFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.thomasphillips3.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -22,5 +25,13 @@ class listFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetail.setOnClickListener {
+            val action = listFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 
 }
