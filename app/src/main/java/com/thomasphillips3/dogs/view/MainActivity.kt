0afae6e51e8a2b2687c9da5bc348386a1a64abb5
Thomas Phillips <thomasphillips3@gmail.com>
2019-12-07 20:32:12 -0800

    add back button to detail view

--- app/src/main/java/com/thomasphillips3/dogs/view/MainActivity.kt
+++ app/src/main/java/com/thomasphillips3/dogs/view/MainActivity.kt
@@ -2,12 +2,24 @@ package com.thomasphillips3.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.thomasphillips3.dogs.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
