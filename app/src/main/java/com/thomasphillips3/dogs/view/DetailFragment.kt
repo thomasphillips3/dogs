027c1f41719547fa80e3e8d8f165ac1215aceeaa
Thomas Phillips <thomasphillips3@gmail.com>
2019-12-13 11:53:28 -0800

    remove tutorial code

--- app/src/main/java/com/thomasphillips3/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/thomasphillips3/dogs/view/DetailFragment.kt
@@ -17,6 +17,8 @@ import kotlinx.android.synthetic.main.fragment_list.*
  */
 class DetailFragment : Fragment() {
 
+    private var dogUuid = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,10 +30,8 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        buttonList.setOnClickListener {
-            val action = DetailFragmentDirections.actionListFragment()
-            Navigation.findNavController(it).navigate(action)
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
         }
     }
-
 }
