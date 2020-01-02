cf252ed14f09ddfba90a0b5979b75f633fc761d1
Thomas Phillips <thomasphillips3@gmail.com>
2020-01-01 23:11:02 -0500

    display images retrieved from back end

--- app/src/main/java/com/thomasphillips3/dogs/view/DogsListAdapter.kt
+++ app/src/main/java/com/thomasphillips3/dogs/view/DogsListAdapter.kt
@@ -7,6 +7,8 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.thomasphillips3.dogs.R
 import com.thomasphillips3.dogs.model.DogBreed
+import com.thomasphillips3.dogs.util.getProgressDrawable
+import com.thomasphillips3.dogs.util.loadImage
 import kotlinx.android.synthetic.main.item_dog.view.*
 
 class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
@@ -36,5 +38,9 @@ class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<D
         holder.view.setOnClickListener {
             Navigation.findNavController(it).navigate(ListFragmentDirections.actionDetailFragment())
         }
+
+        holder.view.imageView.loadImage(
+            dogsList[position].imageUrl,
+            getProgressDrawable(holder.view.imageView.context))
     }
 }
