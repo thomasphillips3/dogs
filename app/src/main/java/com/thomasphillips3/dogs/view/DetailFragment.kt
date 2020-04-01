38b1eb2cbd779fd446edc0269d157c7c89157b83
Thomas Phillips <thomasphillips3@gmail.com>
2020-03-31 23:28:28 -0700

    send dog info via SMS

--- app/src/main/java/com/thomasphillips3/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/thomasphillips3/dogs/view/DetailFragment.kt
@@ -1,10 +1,14 @@
 package com.thomasphillips3.dogs.view
 
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.view.*
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -16,7 +20,10 @@ import com.bumptech.glide.request.transition.Transition
 
 import com.thomasphillips3.dogs.R
 import com.thomasphillips3.dogs.databinding.FragmentDetailBinding
+import com.thomasphillips3.dogs.databinding.SendSmsDialogBinding
+import com.thomasphillips3.dogs.model.DogBreed
 import com.thomasphillips3.dogs.model.DogPalette
+import com.thomasphillips3.dogs.model.SmsInfo
 import com.thomasphillips3.dogs.viewmodel.DetailViewModel
 
 /**
@@ -27,6 +34,7 @@ class DetailFragment : Fragment() {
     private lateinit var viewModel: DetailViewModel
     private var dogUuid = 0
     private var sendSmsStarted = false
+    private var currentDog: DogBreed? = null
 
     private lateinit var dataBinding: FragmentDetailBinding
 
@@ -54,6 +62,7 @@ class DetailFragment : Fragment() {
 
     private fun observeViewModel() {
         viewModel.dogLiveData.observe(this, Observer { dog ->
+            currentDog = dog
             dog?.let {
                 dataBinding.dog = dog
 
@@ -68,7 +77,7 @@ class DetailFragment : Fragment() {
         Glide.with(this)
             .asBitmap()
             .load(url)
-            .into(object: CustomTarget<Bitmap>() {
+            .into(object : CustomTarget<Bitmap>() {
                 override fun onLoadCleared(placeholder: Drawable?) {
                 }
 
@@ -90,18 +99,55 @@ class DetailFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.action_send_sms -> {
                 sendSmsStarted = true
                 (activity as MainActivity).checkSmsPermission()
             }
 
-            R.id.action_share -> { }
+            R.id.action_share -> {
+            }
         }
         return super.onOptionsItemSelected(item)
     }
 
     fun onPermissionResult(permissionGranted: Boolean) {
+        if (sendSmsStarted && permissionGranted) {
+            context?.let {
+                val smsInfo =
+                    SmsInfo(
+                        "",
+                        "${currentDog?.dogBreed} bred for ${currentDog?.bredFor}",
+                        currentDog?.imageUrl
+                    )
 
+                val dialogBinding = DataBindingUtil.inflate<SendSmsDialogBinding>(
+                    LayoutInflater.from(it),
+                    R.layout.send_sms_dialog,
+                    null,
+                    false
+                )
+
+                AlertDialog.Builder(it)
+                    .setView(dialogBinding.root)
+                    .setPositiveButton("Send SMS") { dialog, which ->
+                        if (!dialogBinding.smsDestination.text.isNullOrEmpty()) {
+                            smsInfo.to = dialogBinding.smsDestination.text.toString()
+                            sendSms(smsInfo)
+                        }
+                    }
+                    .setNegativeButton("Cancel") { dialog, which -> }
+                    .show()
+
+                dialogBinding.smsInfo = smsInfo
+            }
+        }
+    }
+
+    private fun sendSms(smsInfo: SmsInfo) {
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+        val sms = SmsManager.getDefault()
+        sms.sendTextMessage(smsInfo.to, null, smsInfo.text, pendingIntent, null)
     }
 }
